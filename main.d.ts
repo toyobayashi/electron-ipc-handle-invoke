e73ca8feb256ba45ceb0595d7b6f727ec96923bb
toyobayashi <lifenglin314@outlook.com>
2020-03-31 23:03:04 +0800

    IpcMainInvokeEvent

--- main.d.ts
+++ main.d.ts
@@ -1,9 +1,13 @@
 import 'electron'
 
 declare module 'electron' {
+  interface IpcMainInvokeEvent extends Event {
+    frameId: number;
+    sender: WebContents;
+  }
   interface IpcMain {
-    handle (channel: string, listener: (event: any, ...args: any[]) => (Promise<void>) | (any)): void
-    handleOnce (channel: string, listener: (event: any, ...args: any[]) => (Promise<void>) | (any)): void
+    handle (channel: string, listener: (event: IpcMainInvokeEvent, ...args: any[]) => (Promise<void>) | (any)): void
+    handleOnce (channel: string, listener: (event: IpcMainInvokeEvent, ...args: any[]) => (Promise<void>) | (any)): void
     removeHandler (channel: string): void
   }
 }
--- renderer.js
+++ renderer.js
@@ -8,7 +8,7 @@ if (!ipcRenderer.invoke) {
 
   ipcRenderer.invoke = ipcRenderer.invoke || function (method, ...args) {
     if (remote.getGlobal('__electron_ipc_handle_invoke__') !== true) {
-      return Promise.reject('@tybys/electron-ipc-handle-invoke/main has not been imported in main process.')
+      return Promise.reject(new Error('@tybys/electron-ipc-handle-invoke/main has not been imported in main process.'))
     }
     return new Promise((resolve, reject) => {
       const id = (new ObjectId()).toString()
